e65cc39517886cf6e4fff035ab00aac5c1cfd6df
towhare <694196619@qq.com>
2020-12-23 00:35:15 +0800

    基本的gpu信息查询

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const { exec } = require('child_process');
 const { promisify } = require('util');
 const execAsync = promisify(exec);
 const gpuTempratureCommand = 'nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader'; // change it for your OS
-const cdCommand = 'C: ';
+/** 按顺序 获取总内存 已用内存 可用内存 温度 gpu占用百分比 */
+const gpuCommand = 'nvidia-smi --format=csv,noheader --query-gpu=memory.total,memory.used,memory.free,temperature.gpu,utilization.gpu ';
 // 尝试运行到c盘
 async function cdToC() {
     try {
@@ -16,34 +17,30 @@ async function cdToC() {
 }
 
 async function getGPUTemperature() {
-    const result = await execAsync(gpuTempratureCommand, {
+    const result = await execAsync(gpuCommand, {
         env: {
             ...process.env,
             Path: 'C:\\Program Files\\NVIDIA Corporation\\NVSMI\\'
         }
     });
     if(result.stdout){
+        let dataArray = result.stdout.replace('\r\n','').split(', ');
+        console.log('共有内存'+dataArray[0]+', 已用'+dataArray[1]+', 可用'+dataArray[2]+',温度:'+dataArray[3]+'°, 利用率:'+dataArray[4]);
         return result.stdout.replace('\r\n','');
     }
 
 }
-// exec(gpuTempratureCommand, {
-//     env: {
-//         ...process.env,
-//         Path: 'C:\\Program Files\\NVIDIA Corporation\\NVSMI\\'
-//     }
-// }, (err, stdout, stderr) => {
-//     console.log('error', err);
-//     console.log('stdout', stdout);
-//     console.log('stderr', stderr);
-// })
 
 async function getTemperature(){
     let temperature = await getGPUTemperature();
-    if(temperature){
-        console.log('温度是'+temperature+'°');
+
+    let gettemp = async () =>{
+        let t = await getGPUTemperature();
     }
+    setInterval(gettemp,1000);
 }
+
+
 getTemperature();
-//listInfomation();
+//getGPUTemperature();
 //console.log('process.env',process.env)
